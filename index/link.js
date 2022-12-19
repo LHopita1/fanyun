ca95aa6c0a96f3507ead54e6ad875608dfca99b7
LHopita1 <120997927+LHopita1@users.noreply.github.com>
2022-12-20 00:27:50 +0800

    Add files via upload

--- index/link.js
+++ index/link.js
@@ -1,89 +1,89 @@
 var iframe=document.querySelector("iframe")
 function practice_submit(){ 
 	if(practice.value == 101) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/practise/0222/0222.html';
+		iframe.src='https://lhopita1.github.io/fanyun/practise/0222/0222.html';
 	if(practice.value == 102) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/practise/0301/0301.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/practise/0301/0301.html'; 
 	if(practice.value == 103) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/practise/0308/0308.html';
+		iframe.src='https://lhopita1.github.io/fanyun/practise/0308/0308.html';
 	if(practice.value == 104) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/practise/0329/0329.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/practise/0329/0329.html'; 
 	if(practice.value == 201) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(practice.value == 202) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(practice.value == 203) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(practice.value == 204) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 } 
 
 function practice_submit_small(){ 
 	if(practice.value == 101) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0222/0222.html';
+		window.location='https://lhopita1.github.io/fanyun/practise/0222/0222.html';
 	if(practice.value == 102) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0301/0301.html'; 
+		window.location='https://lhopita1.github.io/fanyun/practise/0301/0301.html'; 
 	if(practice.value == 103) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0308/0308.html'; 
+		window.location='https://lhopita1.github.io/fanyun/practise/0308/0308.html'; 
 	if(practice.value == 104) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0329/0329.html';
+		window.location='https://lhopita1.github.io/fanyun/practise/0329/0329.html';
 	if(practice.value == 201) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(practice.value == 202) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(practice.value == 203) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(practice.value == 204) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 } 
 
 function homework_submit(){ 
 	if(homework.value == 201) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(homework.value == 202) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(homework.value == 203) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(homework.value == 204) 
-		iframe.src='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		iframe.src='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 } 
 
 function homework_submit_small(){ 
 	if(homework.value == 201) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(homework.value == 202) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(homework.value == 203) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(homework.value == 204) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 } 
 
 function practice_submit_sm(){ 
 	if(practice_sm.value == 101) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0222/0222.html';
+		window.location='https://lhopita1.github.io/fanyun/practise/0222/0222.html';
 	if(practice_sm.value == 102) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0301/0301.html'; 
+		window.location='https://lhopita1.github.io/fanyun/practise/0301/0301.html'; 
 	if(practice_sm.value == 103) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0308/0308.html'; 
+		window.location='https://lhopita1.github.io/fanyun/practise/0308/0308.html'; 
 	if(practice_sm.value == 104) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/practise/0329/0329.html';
+		window.location='https://lhopita1.github.io/fanyun/practise/0329/0329.html';
 	if(practice_sm.value == 201) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(practice_sm.value == 202) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(practice_sm.value == 203) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(practice_sm.value == 204) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 }
 function homework_submit_sm(){ 
 	if(homework_sm.value == 201) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_img/change_font_img.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_img/change_font_img.html'; 
 	if(homework_sm.value == 202) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/change_font_code/change_font_code.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/change_font_code/change_font_code.html'; 
 	if(homework_sm.value == 203) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/calculus/teaching_calculus.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/calculus/teaching_calculus.html'; 
 	if(homework_sm.value == 204) 
-		window.location='http://myweb.ncyu.edu.tw/~s1082574/homework/midterm/teaching_numerical_analysis.html'; 
+		window.location='https://lhopita1.github.io/fanyun/homework/midterm/teaching_numerical_analysis.html'; 
 }
